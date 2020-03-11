31f9e193e952c14e9eed49fbc1b0cf235985b4bd
franjaku <kulyckyf@umich.edu>
2020-03-10 17:35:38 -0700

    resolve path

--- bookapp.py
+++ bookapp.py
@@ -5,6 +5,25 @@ from bookdb import BookDB
 DB = BookDB()
 
 
+def resolve_path(path):
+    funcs = {
+        '': books,
+        'book': book,
+    }
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
+
+    return funcs, args
+
+
 def book(book_id):
     return "<h1>a book with id %s</h1>" % book_id
 
@@ -14,10 +33,25 @@ def books():
 
 
 def application(environ, start_response):
-    status = "200 OK"
     headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    return ["<h1>No Progress Yet</h1>".encode('utf8')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-Length', str(len(body))))
+        start_response(status, headers)
+        return body.encode('utf8')
 
 
 if __name__ == '__main__':
